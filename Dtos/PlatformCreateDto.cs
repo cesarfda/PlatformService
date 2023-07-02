190ae2e9740c20971c010d66ee56e2c1aacb63e4
Cesar Araujo <ara17008@byui.edu>
2023-07-01 17:54:47 -0700

    mapping

--- Dtos/PlatformCreateDto.cs
+++ Dtos/PlatformCreateDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PlatformService.Dtos
 {
-    public class platformCreateDto
+    public class PlatformCreateDto
     {
         [Required]
         public string Name { get; set; }
